6245fe83ee1d64fafd66314386918cf679e6494b
inco2 <info@inco.2fh.co>
2016-07-05 01:07:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('So you want to incorporate a non profit entity? Just say YES to get started.')
                 .then(() => 'speak');
         }
     },
